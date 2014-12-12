72372afd61551332e287f1863c2fb2d27b18d1c5
Christoph Jobst <cjobst@wifa.uni-leipzig.de>
2014-12-12 18:37:24 +0100

    Add SMILES-preview for participant, later usable for automatical grading

--- assJSMEQuestion/plugin.php
+++ assJSMEQuestion/plugin.php
@@ -4,16 +4,21 @@
 $id = "assJSMEQuestion";
  
 // code version; must be changed for all code changes
-$version = "1.1.1";
+$version = "1.1.2";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.3.0";
 $ilias_max_version = "4.3.999";
 
-// add the responsible person for the plugin
-$responsible = "Yves Annanias";
-$responsible_mail = "yves.annanias@llz.uni-halle.de";
+//...-09/2014:
+//$responsible = "Yves Annanias";
+//$responsible_mail = "yves.annanias@llz.uni-halle.de";
+
+//10/2014-...
+$responsible = "Christoph Jobst";
+$responsible_mail = "christoph.jobst@llz.uni-halle.de";
+
 //  Dr. Peter Ertl: 
 //  http://peter-ertl.com/ 
 //  http://peter-ertl.com/jsme/index.html
